850f1f0170ca2d3593be4cb7482769e89ed4d08d
Mario Rodriguez <mariordgez@hotmail.com>
2021-05-20 11:43:07 -0500

    Added spec for player_hot and array methods

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -27,5 +27,29 @@ it 'Should not print the string not + compare_output' do
   end
 
 end
+describe '#season_avr_arr' do
+it 'Should return an array with the arguments passed to the method' do
+expect(report.season_avr_arr(50,20,20,0.45)).to eql([50,20,20,0.45])
+end
+it 'Should not return an array with the arguments on different order' do
+  expect(report.season_avr_arr(50,20,20,0.45)).not_to eql([20,50,20,0.45])
+  end
+end
+describe '#last_ten_arr' do
+it 'Should return an array with the arguments passed to the method' do
+expect(report.last_ten_arr(50,20,20,0.45)).to eql([50,20,20,0.45])
+end
+it 'Should not return an array with the arguments on different order' do
+  expect(report.last_ten_arr(50,20,20,0.45)).not_to eql([20,50,20,0.45])
+  end
+end
+describe '#player_hot?' do
+it 'should return true if arg is <-5' do
+expect(report.player_hot?(-10)).to eql(true)
+end
+it 'should not return true if arg is >-5' do
+  expect(report.player_hot?(-4)).not_to eql(true)
+  end
 
+end
 end
